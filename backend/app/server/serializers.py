640beb2da32d04225af2d37ec51010c935747823
josh1506 <jmjabor@gmail.com>
2024-05-25 14:56:23 +0800

    [Reusable] Server channels list. Channel is relation to Server

--- backend/app/server/serializers.py
+++ backend/app/server/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from app.server.models import Server
+from app.server.models import Server, Channel
+
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = '__all__'
 
 
 class ServerSerializer(serializers.ModelSerializer):
+    channel_server = ChannelSerializer(many=True, read_only=True)
+
     class Meta:
         model = Server
         fields = '__all__'
