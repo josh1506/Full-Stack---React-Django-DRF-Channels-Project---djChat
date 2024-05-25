83bd64d844480d472867a8d2433a84664d0c2ac8
josh1506 <jmjabor@gmail.com>
2024-05-25 15:14:59 +0800

    [Reusable] Serializer annotation and modify data

--- backend/app/server/serializers.py
+++ backend/app/server/serializers.py
@@ -11,7 +11,21 @@ class ChannelSerializer(serializers.ModelSerializer):
 
 class ServerSerializer(serializers.ModelSerializer):
     channel_server = ChannelSerializer(many=True, read_only=True)
+    num_members = serializers.SerializerMethodField()
 
     class Meta:
         model = Server
-        fields = '__all__'
+        exclude = ('member', )
+
+    def get_num_members(self, obj):
+        if hasattr(obj, "num_members"):
+            return obj.num_members
+        # return obj.member.count()
+        return None
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        num_members = self.context.get('num_members')  # Reference to views,py context={"num_members": with_num_members}
+        if not num_members:
+            data.pop('num_members', None)
+        return data
--- backend/app/server/views.py
+++ backend/app/server/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets
@@ -14,17 +15,21 @@ class ServerListViewSet(viewsets.ViewSet):
     def list(self, request):
         category = request.query_params.get('category')
         qty = request.query_params.get('qty')
-        by_user = request.query_params.get('by_user') == "true"
+        by_user = request.query_params.get('by_user', 'false').lower() == "true"
         by_server_id = request.query_params.get('by_server_id')
+        with_num_members = request.query_params.get('with_num_members', 'false').lower() == "true"
 
         if by_user is True and not request.user.is_authenticated:
             raise AuthenticationFailed()
 
-        if category:
+        if category is not None:
             self.queryset = self.queryset.filter(category__name=category)
 
         if qty is True:
             self.queryset = self.queryset[: int(qty)]
+
+        if with_num_members is True:
+            self.queryset = self.queryset.annotate(num_members=Count('members'))
 
         if by_server_id is not None:
             self.queryset = [get_object_or_404(Server, pk=by_server_id)]
@@ -33,6 +38,6 @@ class ServerListViewSet(viewsets.ViewSet):
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members": with_num_members})
         return Response(serializer.data)
 
