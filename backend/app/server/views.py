4da51b983be7b2183ec213f949eab2749eef1a30
josh1506 <jmjabor@gmail.com>
2024-05-25 14:25:53 +0800

    Add filter by user or quantity

--- backend/app/server/views.py
+++ backend/app/server/views.py
@@ -10,9 +10,18 @@ class ServerListViewSet(viewsets.ViewSet):
 
     def list(self, request):
         category = request.query_params.get('category')
+        qty = request.query_params.get('qty')
+        by_user = request.query_params.get('by_user') == "true"
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
+
+        if qty is True:
+            self.queryset = self.queryset[: int(qty)]
+
+        if by_user is True:
+            user_id = request.user.id
+            self.queryset = self.queryset.filter(member=user_id)
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
