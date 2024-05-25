7983c0abcdd467e2cf90a91c3a213450ab9e3bab
josh1506 <jmjabor@gmail.com>
2024-05-25 14:48:16 +0800

    Filter server by id and by_user required authentication

--- backend/app/server/views.py
+++ backend/app/server/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import viewsets
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 
 from app.server.models import Server, Category
@@ -12,12 +15,19 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get('category')
         qty = request.query_params.get('qty')
         by_user = request.query_params.get('by_user') == "true"
+        by_server_id = request.query_params.get('by_server_id')
+
+        if by_user is True and not request.user.is_authenticated:
+            raise AuthenticationFailed()
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
         if qty is True:
             self.queryset = self.queryset[: int(qty)]
+
+        if by_server_id is not None:
+            self.queryset = [get_object_or_404(Server, pk=by_server_id)]
 
         if by_user is True:
             user_id = request.user.id
