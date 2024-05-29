56426be37bd0b7c8ef8fc1d0af9f447aa6e00794
josh1506 <jmjabor@gmail.com>
2024-05-29 17:22:53 +0800

    Implement primary drawer for chat servers

---
+++ frontend/src/components/PrimaryDraw/DrawToggle.tsx
@@ -0,0 +1,14 @@
+import {Box, IconButton} from "@mui/material";
+import {ChevronLeft} from "@mui/icons-material";
+
+const DrawToggle = () => {
+    return (
+        <Box sx={{height: "50px", display: "flex", justifyContent: "center", placeItems: "center"}}>
+            <IconButton>
+                <ChevronLeft/>
+            </IconButton>
+        </Box>
+    )
+}
+
+export default DrawToggle
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import {Box, CssBaseline} from "@mui/material";
+
 import PrimaryAppBar from "./templates/PrimaryAppBar.tsx";
+import PrimaryDraw from "./templates/PrimaryDraw.tsx";
 
 const Home = () => {
     return (
         <Box sx={{display: "flex"}}>
             <CssBaseline/>
             <PrimaryAppBar/>
+            <PrimaryDraw>
+                asdasdasd
+            </PrimaryDraw>
         </Box>
     )
 }
