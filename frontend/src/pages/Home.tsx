94926d3d1bc9addd3e289b4f74e14d686d576db5
josh1506 <jmjabor@gmail.com>
2024-05-30 14:11:21 +0800

    Implement secondary drawer for category

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import {Box, CssBaseline} from "@mui/material";
 
 import PrimaryAppBar from "./templates/PrimaryAppBar.tsx";
 import PrimaryDraw from "./templates/PrimaryDraw.tsx";
+import SecondaryDraw from "./templates/SecondaryDraw.tsx";
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <PrimaryDraw>
                 asdasdasd
             </PrimaryDraw>
+            <SecondaryDraw/>
         </Box>
     )
 }
