b33fdfaf03625859d29135d7093eb4ed30f92c3b
josh1506 <jmjabor@gmail.com>
2024-06-01 22:39:40 +0800

    Implement reusable fetch hook

---
+++ frontend/src/hooks/useCrud.ts
@@ -0,0 +1,41 @@
+import {useState} from "react";
+
+import useAxiosWithInterceptor from "../helpers/jwtinterceptor.ts";
+import {BASE_URL} from "../config.ts";
+import {Error} from "@mui/icons-material";
+
+interface IuseCrud<T> {
+    dataCRUD: T[]
+    fetchData: () => Promise<void>
+    error: Error | null
+    isLoading: boolean
+}
+
+const useCrud = <T>(initialData: T[], apiURL: string): IuseCrud<T> => {
+    const jwtAxios = useAxiosWithInterceptor()
+    const [dataCRUD, setDataCRUD] = useState<T[]>(initialData)
+    const [error, setError] = useState<Error | null>(null)
+    const [isLoading, setIsLoading] = useState(false)
+
+    const fetchData = async () => {
+        setIsLoading(true)
+        try {
+            const response = await jwtAxios.get(`${BASE_URL}${apiURL}`, {})
+            const data = response.data
+            setDataCRUD(data)
+            setError(null)
+            setIsLoading(false)
+            return data
+        } catch (err: any) {
+            if (err.response && err.response.status === 400) {
+                setError(new Error("400"))
+            }
+            setIsLoading(false)
+            throw err
+        }
+    }
+
+    return {fetchData, dataCRUD, error, isLoading}
+}
+
+export default useCrud
